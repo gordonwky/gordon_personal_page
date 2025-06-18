cf31500d0fe6c356b09e1374113d2619f19bcb71
Gordon Wong <gordon.wong@vretta.com>
2025-06-18 16:37:00 -0400

    config change

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath : process.env.NODE_ENV === 'production' ? '/gordon_personal_page' :'', 
-  assetPrefix: process.env.NODE_ENV === 'production'  ? '/gordon_personal_page' : '',  
+  basePath : process.env.NODE_ENV === 'production' ? '/portfolio' :'', 
+  assetPrefix: process.env.NODE_ENV === 'production'  ? '/portfolio' : '',  
   output: 'export',
     images: {
     unoptimized: true
